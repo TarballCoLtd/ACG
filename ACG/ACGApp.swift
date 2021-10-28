7555300991e7dd65571e5943198bf91e1511b395
Alyx Ferrari <rubiquikyt@gmail.com>
2021-10-28 18:50:10 -0400

    Initial Commit

---
+++ ACG/ACGApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACGApp.swift
+//  ACG
+//
+//  Created by Alyx Ferrari on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct ACGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ACG/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ACG
+//
+//  Created by Alyx Ferrari on 10/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
