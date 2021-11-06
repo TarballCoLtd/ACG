546507a598cb5579f933553e29159fa34b2151bb
Alyx Ferrari <rubiquikyt@gmail.com>
2021-11-06 19:30:47 -0400

    Add ScrollView

---
+++ ACG/WirelessPicker.swift
@@ -0,0 +1,20 @@
+//
+//  WirelessPicker.swift
+//  ACG
+//
+//  Created by Alyx Ferrari on 11/6/21.
+//
+
+import SwiftUI
+
+struct WirelessPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WirelessPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        WirelessPicker()
+    }
+}
