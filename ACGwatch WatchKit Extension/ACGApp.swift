a7bd26ee66bd7e98b7f437bee2f464a28cce9320
Alyx Ferrari <rubiquikyt@gmail.com>
2021-11-17 21:08:24 -0500

    Add watchOS target

---
+++ ACGwatch WatchKit Extension/ACGApp.swift
@@ -0,0 +1,19 @@
+//
+//  ACGApp.swift
+//  ACGwatch WatchKit Extension
+//
+//  Created by Alyx Ferrari on 11/17/21.
+//
+
+import SwiftUI
+
+@main
+struct ACGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ ACGwatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ACGwatch WatchKit Extension
+//
+//  Created by Alyx Ferrari on 11/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
